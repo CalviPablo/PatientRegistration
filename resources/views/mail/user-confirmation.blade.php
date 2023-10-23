55f93a16faef265f406db31062049ce90f39d309
CalviPablo <pablo.calvi97@gmail.com>
2023-10-22 23:12:54 -0300

    env

--- resources/views/mail/user-confirmation.blade.php
+++ resources/views/mail/user-confirmation.blade.php
@@ -1,11 +1,54 @@
-
-# Confirm Your Registration
-
-Thank you for registering with our website! To complete your registration, please click the button below to confirm your email address.
-
-
-If you did not request this registration, you can safely ignore this email.
-
-Thanks,<br>
-{{ config('app.name') }}
-
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        /* Add your inline CSS styles here */
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            background-color: #fff;
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+        .header {
+            background-color: #3498db;
+            color: #fff;
+            text-align: center;
+            padding: 20px;
+        }
+        .button {
+            display: block;
+            width: 200px;
+            background-color: #3498db;
+            color: #fff;
+            text-align: center;
+            text-decoration: none;
+            padding: 10px 20px;
+            margin: 20px auto;
+            border-radius: 5px;
+        }
+        .footer {
+            text-align: center;
+            color: #777;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h2>The patient has been succesfully registered.</h2>
+        </div>
+        <p>Thank you for registering with our website! To complete your registration, please click the button below to confirm your email address.</p>
+        <p>Thanks,<br>{{ config('app.name') }}</p>
+        <div class="footer">
+            Patient Registration
+        </div>
+    </div>
+</body>
+</html>
